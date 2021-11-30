8849764a1edde2fb29fcd4a9f0c8d3836f442da2
farukmert <60607212+farukmert@users.noreply.github.com>
2021-11-30 09:53:37 +0300

    database oluşturmak için gerekli sql querylerini yüklemeyi unuttuğumuz için şimdi yüklüyoruz

---
+++ dataekle.sql
@@ -0,0 +1,11 @@
+insert into Personel values ('4565','erkek','5000','11/05/2001','mert','ekin','lise','46362635467','12312456310','06/07/2021','12312309810','ankara','mert.ekin@hotmail.com','Kullanici')
+insert into Personel values ('2365','kadin','3000','09/11/2000','faruk','akcan','üniversite','26353546576','12372312310','12/10/2018','12383912310','ankara','faruk.akcan@hotmail.com','Kullanici')
+insert into Personel values ('4875','erkek','8000','08/12/1999','kaan','deniz','ortaokul','82745893621','12312385910','10/11/2015','12312312678','ankara','kaan.deniz@hotmail.com','Kullanici')
+
+
+insert into Kullanici values('mert','MoC8yU7TqCjbo9FxVlEYiQ==','Admin','4565')
+insert into Kullanici values('faruk','MoC8yU7TqCjbo9FxVlEYiQ==','Servis','2365')
+insert into Kullanici values('kaan','MoC8yU7TqCjbo9FxVlEYiQ==','Sefer','4875')
+
+--delete from kullanici where personelID='4565'
+--delete from personel where personelID='4565'
